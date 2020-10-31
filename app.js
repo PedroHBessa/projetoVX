877e74c6db66da13911644f4efc0d93cb3ebb729
Pedro <pedro.hbessa@live.com>
2020-10-30 23:01:19 -0300

    atualização com uso de objetos

--- app.js
+++ app.js
@@ -1,80 +1,93 @@
+//////////////////VAR CREATION///////////////////////////////
 
 
-function disparaFuncaoOption1(){
 
-	var imagem = document.querySelector(".imagem");
-var texto = document.querySelector(".texto");
-var linhas = document.querySelector(".linhas");
-var dividir = document.querySelector(".dividir");
-var divpai = document.querySelector(".divpai");
-var select = document.querySelector("#list");
 
-	imagem.style.display = "none";
-	texto.style.display = "none";
-	linhas.style.display = "none";
-	dividir.style.display = "none";
+//////////////////FUNCTION CREATION////////////////////////////
+	
+	function formTrigger(){
+		var tr1Pai = document.querySelector('.tr1Pai');
+		tr1Pai.innerHTML = "";
+		var selectForm = document.querySelector('.selectForm').value;
+		var numTr = 0;
+		
+		for( var i = 0; i < selectForm ; i++  ){
+			
+			numTr++;
+			var form = 'TR'+numTr+':<div class="tr1"><form class="form"> <select id="list" name="list" onchange="optionTrigger();"><option selected disabled="disabled" ></option><option value="image">Image</option><option value="text">Text</option><option value="line">Line</option><option value="split">Split</option> </select></form></div>'
+			
+			$(tr1Pai).append(form);
 
-	if(select.value === "imagem"){
-		imagem.style.display = "block";
-		$(".tr1Pai").append(imagem);
 		}
+		
+	}
 
-		else if(select.value === "texto"){
-		texto.style.display = "block";
-		$(".tr1mPai").append(texto);	
-		}
+	function optionTrigger(){
+		
+		
+		
+		$('.formImage').remove();
+		$('.formText').remove();
+		$('.formLine').remove();
+		$('.formSplit').remove();
 
-		else if(select.value === "linhas"){
-		linhas.style.display = "block";
-		$(".tr1Pai").append(linhas);	
-		}
+		select = document.querySelector("#list");
 
-		else if(select.value === "dividir"){
-		dividir.style.display = "block";
-		$(".tr1Pai").append(dividir);	
+		if(select.value === "image"){
+			addImgBlock();}
+			if(select.value === "text"){
+			addTxtBlock();}
+			if(select.value === "line"){
+			addLineBlock();}
+			if(select.value === "split"){
+			addSplitBlock();}
 		}
+	
+
+	 function addImgBlock(){
+	 	 $(".blockRoot").append(divBlock.imgBlock);
+	 }
+
+	 function addTxtBlock(){
+	 	 $(".blockRoot").append(divBlock.txtBlock);
+	 }
+
+	 function addLineBlock(){
+	 	 $(".blockRoot").append(divBlock.lineBlock);
+	 }
+
+	 function addSplitBlock(){
+	 	 $(".blockRoot").append(divBlock.splitBlock);
+	 }
+
+	 //////////////OBJECTS CREATION///////////////////////
+
+	 var imageObj = {
+	 	
+	 	tdWidth: 600,	 	
+	 	imgName: "shape_top",
+	 	imgExt:".png'",	 	
+	 	imgWidth: 600,
+	 	imgHeight: 33,
+
+	 	
+	 	concatImgObj: function(){
+	 		var imageHTML = "<tr><td> width='"+this.tdWidth+"'><img src='"+this.imgName+"."+this.imgExt+"' width='"+this.imgWidth+"' height='"+this.imgHeight+"' style='display: block; border: 0;' alt=''></td></tr>"
+	 		return imageHTML;
+	 	}
+
+	 }
+
+	 var divBlock = {
+	 	imgBlock: '<form class="formImage"><div class="imgBlock"><div class="imagem"> <br> <label for="imgName">Image Name:</label> <input type="text" id="" /><input type="radio" name="extension" /> <label for="extension">.jpg</label> <input type="radio" name="extension" /> <label for="extension">.png</label> <br><br> <label for="link">LINK SRC:</label> <input type="text" id="" /> <br><br><button>Send</button></form></div>',
+	 	txtBlock: '<form class="formText"><div class="txtBlock"> <br> <label for="text">Text:</label> <input type="text" /><input type="radio" name="fontFamily" /> <label for="fontFamily">globotipo_textoblack</label> <input type="radio" name="fontFamily" /> <label for="fontFamily">globotipo_textoregular</label> <br><br><label for="fontSize">Font Size:</label> <input type="text" /> <br><br> <label for="lineHeight">Line Height:</label> <input type="text" /> <br><br> <label for="color">Color:</label> <input type="text" /> <br><br> <label for="textAlign">Text Align:</label> <input type="text" /> <br><br><button>Send</button></div></form>',
+	 	lineBlock: '<form class="formLine"><div class="lineBlock"> <br> <label for="lineHeight">Line Height:</label> <input type="text" /> <br><br><button>Send</button></div></form>',
+ 	 	splitBlock: '<form class="formSplit"><div class="splitBlock"> <br> <label for="tr">TR:</label> <select id="" name=""><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option> </select> <br><br> <label for="tr">TD:</label><select id="" name=""><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option> </select> <br><br><button>Send</button></div></form>'
 	 }
 
 
 
-	 function disparaFuncaoOption2(){
-
-	var imagem2 = document.querySelector(".imagem2");
-var texto2 = document.querySelector(".texto2");
-var linhas2 = document.querySelector(".linhas2");
-var dividir2 = document.querySelector(".dividir2");
-var divpai2 = document.querySelector(".divpai2");
-var select2 = document.querySelector("#list2");
 
 
 
-	imagem2.style.display = "none";
-	texto2.style.display = "none";
-	linhas2.style.display = "none";
-	dividir2.style.display = "none";
-
-	if(select2.value === "imagem2"){
-		imagem2.style.display = "block";
-		$(".tr2Pai").append(imagem2);
-		}
-
-		else if(select2.value === "texto2"){
-		texto2.style.display = "block";
-		$(".tr2Pai").append(texto2);	
-		}
-
-		else if(select2.value === "linhas2"){
-		linhas2.style.display = "block";
-		$(".tr2Pai").append(linhas2);	
-		}
-
-		else if(select2.value === "dividir2"){
-		dividir2.style.display = "block";
-		$(".tr2Pai").append(dividir2);	
-		}
-	 }
-
-
-
-
-
+      
